5d5d8afd23389e9b90362898df28a401f5189049
amricko0b <amricko0b@gmail.com>
2022-07-14 17:21:50 +0500

    #1: Typesafe JSON-RPC method description

--- core/src/main/scala/sttp/tapir/jsonrpc/package.scala
+++ core/src/main/scala/sttp/tapir/jsonrpc/package.scala
@@ -2,6 +2,29 @@ package sttp.tapir
 
 package object jsonrpc {
 
+  /** Describe a JSON-RPC method with no result - so called notification. Description comes without params, one should describe params
+    * later.
+    *
+    * @param name
+    *   name for a method
+    * @return
+    *   basic method description
+    */
+  def jsonRpcMethod(name: String): JsonRpcMethod[Unit, Unit] =
+    JsonRpcMethod(name, JsonRpcParams.Empty, JsonRpcResult.Empty)
+
+  /** Describe a JSON-RPC method with a result. Description comes without params, one should describe params later.
+    *
+    * @param name
+    *   name for a method
+    * @param result
+    *   result is necessary, so we don't provide fluent syntax to describe it
+    * @return
+    *   basic method description
+    */
+  def jsonRpcMethod[T](name: String, result: JsonRpcResult[T]): JsonRpcMethod[Unit, T] =
+    JsonRpcMethod(name, JsonRpcParams.Empty, result)
+
   /** Describe a JSON-RPC param. Only basic information - type and name. No documentation provided on this step.
     *
     * @param name
